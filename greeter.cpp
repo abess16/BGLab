935c8ff693b9a526a7f55b7a6f86e4d012497e00
abess16 <s6abgebr@uni-bonn.de>
2017-06-20 11:46:05 +0200

    underline output

--- greeter.cpp
+++ greeter.cpp
@@ -5,11 +5,11 @@ using namespace std;
 void greet(std::string name)
 {
    	cout<<"hello world"<<name<<endl;
-	
+	cout<<"======================="<<endl;
 }
 
 void bye(std::string name)
 {
 	cout<<"bye world"<<name<<endl;
-	 
+	cout<<"======================="<<;
 }
